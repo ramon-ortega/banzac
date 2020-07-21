4bc5ebcf175191208f959c006b289181e1042c96
UxorialColt <ramortega1594@gmail.com>
2020-07-21 18:45:32 -0500

    Agregamos la logica de mostrar dinero disponible en cajero y la logica de depositos modificando la base de datos

--- transacciones/models.py
+++ transacciones/models.py
@@ -11,13 +11,13 @@ class Transaccion(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     profile = models.ForeignKey('users.Profile', on_delete=models.CASCADE)
 
-    saldo = models.DecimalField(max_digits=7, decimal_places=2, default=0)
-    transferencia = models.DecimalField(max_digits=6, decimal_places=2)
-    retiro = models.DecimalField(max_digits=6, decimal_places=2)
+    saldo = models.FloatField(default=0)
+    transferencia = models.FloatField(null = True)
+    retiro = models.FloatField(null=True)
 
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        """Return title and username"""
+        """Return username"""
         return '{}'.format(self.user.username)
--- transacciones/views.py
+++ transacciones/views.py
@@ -5,19 +5,61 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
 
-saldo=0
+# Models 
+from transacciones.models import Transaccion
+from django.contrib.auth.models import User
 
 @login_required
 def solicitar_saldo(request):
     """Return saldo"""
-    return render(request, 'transacciones/saldo.html')
+
+    x = Transaccion.objects.filter(profile_id = request.user.id)
+    numero_deposito = len(x)
+
+    saldo_transaccion = Transaccion.objects.get(id = numero_deposito)
+    saldo = saldo_transaccion.saldo
+
+    return render(
+        request = request,
+        template_name = 'transacciones/saldo.html',
+        context = {
+            'saldo': saldo
+        }
+    )
 
 @login_required
 def retiro(request):
+    """ Agregamos Retiro """
+
+    x = Transaccion.objects.filter(profile_id = request.user.id)
+    numero_deposito = len(x)
+    saldo_transaccion = Transaccion.objects.get(id = numero_deposito)
+    saldo = saldo_transaccion.saldo
+
+    if request.method == 'POST':
+        retiro = float(request.POST['retiro'])
+        saldo = saldo - retiro
+
+    import pdb; pdb.set_trace()
+
+
     return render(request, 'transacciones/retiro.html')
 
 @login_required
 def deposito(request):
+    """Agregamos deposito""" 
+    x = Transaccion.objects.filter(profile_id = request.user.id)
+    numero_deposito = len(x)
+
+    saldo_transaccion = Transaccion.objects.get(id = numero_deposito)
+    saldo = saldo_transaccion.saldo
+    if request.method == 'POST':
+        deposito = float(request.POST['deposito'])
+        saldo = saldo + deposito
+
+        transaccion = Transaccion(saldo=saldo, transferencia=deposito, retiro=0, profile_id = user.id, user_id = user.id)
+        transaccion.save()
+
     return render(request, 'transacciones/deposito.html')
 
 @login_required
