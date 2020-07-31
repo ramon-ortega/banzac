5ae3c11643ec9482f6a9f1a5621dc36a6cbe35f4
UxorialColt <ramortega1594@gmail.com>
2020-07-31 14:46:13 -0500

    Agregamos al dashboard de admin las transacciones

--- transacciones/admin.py
+++ transacciones/admin.py
@@ -1,3 +1,29 @@
+""" Transacciones admin classes """
+
+# Django
 from django.contrib import admin
 
-# Register your models here.
+# Models
+from transacciones.models import Transaccion
+
+@admin.register(Transaccion)
+class TransaccionAdmin(admin.ModelAdmin):
+    """Transaccion Admin."""
+    list_display = ('id', 'user', 'transferencia', 'retiro')
+    list_display_links = ('id', 'user')
+    search_fields = ('user__email', 'user__first_name', 'user__last_name', 'user__username')
+    list_filter = ('created', 'modified')
+
+    fieldsets = (
+        ('Transaccion',{
+            'fields': (
+                ('user', 'transferencia', 'retiro'),
+            ),
+        }),
+        ('Metadata',{
+            'fields': (
+                ('created', 'modified'),
+            )
+        }),
+    )
+    readonly_fields = ('created', 'modified')
